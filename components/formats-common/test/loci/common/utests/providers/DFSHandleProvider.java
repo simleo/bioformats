d92a480ec588f08f4f20b6f93b84e47308f5a21c
simleo <simleo@crs4.it>
2014-10-21 17:04:28 +0100

    added DFSHandleProvider

---
+++ components/formats-common/test/loci/common/utests/providers/DFSHandleProvider.java
@@ -0,0 +1,75 @@
+/*
+ * #%L
+ * Common package for I/O and related utilities
+ * %%
+ * Copyright (C) 2005 - 2014 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package loci.common.utests.providers;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataOutputStream;
+
+import loci.common.IRandomAccess;
+import loci.common.DFSHandle;
+
+/**
+ * Implementation of IRandomAccessProvider that produces instances of
+ * loci.common.DFSHandle.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/common/test/loci/common/utests/providers/DFSHandleProvider.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/common/test/loci/common/utests/providers/DFSHandleProvider.java;hb=HEAD">Gitweb</a></dd></dl>
+ *
+ * @see IRandomAccessProvider
+ * @see loci.common.DFSHandle
+ */
+class DFSHandleProvider implements IRandomAccessProvider {
+
+  public IRandomAccess createMock(
+      byte[] page, String mode, int bufferSize) throws IOException {
+    String name = UUID.randomUUID().toString();
+    Path path = new Path(name);
+    Configuration conf = new Configuration();
+    FileSystem fs = FileSystem.get(conf);  // FIXME: use MiniDFSCluster
+    FSDataOutputStream stream = fs.create(path);
+    try {
+      stream.write(page);
+    } finally {
+      stream.close();
+    }
+    fs.deleteOnExit(path);
+    return new DFSHandle(name, mode);  // TODO: bufferSize
+  }
+
+}
